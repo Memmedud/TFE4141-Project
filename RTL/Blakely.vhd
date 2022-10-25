ad41db8cab5f5f5ef4874cb2906412a152374013
krissch97 <krissch97@gmail.com>
2022-10-25 14:11:34 +0200

    Update Blakely.vhd

--- RTL/Blakely.vhd
+++ RTL/Blakely.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity blakely is
     generic (
@@ -25,8 +26,8 @@ signal result_bitshift  : STD_LOGIC_VECTOR(C_block_size - 1 downto 0);
 
 signal sum_a            : STD_LOGIC_VECTOR(C_block_size - 1 downto 0);
 signal mux_bi           : STD_LOGIC_VECTOR(C_block_size - 1 downto 0);
-signal sum_n            : STD_LOGIC_VECTOR(C_block_size - 1 downto 0);
-signal sum_2n           : STD_LOGIC_VECTOR(C_block_size - 1 downto 0);
+signal sum_n            : STD_LOGIC_VECTOR(C_block_size downto 0);
+signal sum_2n           : STD_LOGIC_VECTOR(C_block_size downto 0);
 
 signal overflow1        : STD_LOGIC;
 signal overflow2        : STD_LOGIC;
@@ -45,7 +46,28 @@ begin
     -- Combinatorial datapath
     process(a, bi, nega_n, nega_2n, result, result_nxt, result_bitshift, sum_a, mux_bi, sum_n, sum_2n, overflow1, overflow2)
     begin
-        
+        result_bitshift <= shift_left(result, 1);
+        sum_a <= result_bitshift + a;
+        if(bi = '1') then
+            mux_bi <= sum_a;
+        else
+            mux_bi <= result_bitshift;
+        end if;
+        sum_n   <= STD_LOGIC_VECTOR(signed(nega_n) + signed(mux_bi));
+        sum_2n  <= STD_LOGIC_VECTOR(signed(nega_2n) + signed(mux_bi));
+        overflow1 <= sum_n(C_block_size);
+        overflow2 <= sum_2n(C_block_size);
+
+        if(overflow1 = '1' AND overflow2 = '1') then
+            result_nxt <= mux_bi;
+        elsif(overflow1 = '0' AND overflow2 = '1') then
+            result_nxt <= sum_n(C_block_size - 1 downto 0);
+        elsif(overflow1 = '0' AND overflow2 = '0') then
+            result_nxt <= sum_2n(C_block_size - 1 downto 0);
+        else
+            result_nxt <= (others => '0');
+        end if;
+
 
     end process;
 end rtl;
