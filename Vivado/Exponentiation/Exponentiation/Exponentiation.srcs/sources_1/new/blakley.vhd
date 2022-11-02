59531e577329531885be6f3e4606055bedeb544a
Mathias Støle <mathst23@gmail.com>
2022-11-02 21:02:01 +0100

    Blakely is up and running

--- Vivado/Exponentiation/Exponentiation/Exponentiation.srcs/sources_1/new/blakley.vhd
+++ Vivado/Exponentiation/Exponentiation/Exponentiation.srcs/sources_1/new/blakley.vhd
@@ -1,39 +1,49 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.math_real.all;
 
 entity blakely is
     generic (
         C_block_size : integer := 256
     );
     port (
-        -- Inputs
-        clk         :  in STD_LOGIC;
-        rst_n       :  in STD_LOGIC;
+        -- Clock and reset
+        clk             :  in STD_LOGIC;
+        rst_n           :  in STD_LOGIC;
 
-        a           :  in STD_LOGIC_VECTOR(C_block_size - 1 downto 0);
-        bi          :  in STD_LOGIC;                                         -- From PISO register
-        nega_n      :  in STD_LOGIC_VECTOR(C_block_size - 1 downto 0);
-        nega_2n     :  in STD_LOGIC_VECTOR(C_block_size - 1 downto 0);
+        -- Inputs
+        a               :  in STD_LOGIC_VECTOR(C_block_size - 1 downto 0);
+        b               :  in STD_LOGIC_VECTOR(C_block_size - 1 downto 0);
+        n               :  in STD_LOGIC_VECTOR(C_block_size - 1 downto 0);
+        n_shifted       :  in STD_LOGIC_VECTOR(C_block_size downto 0);
+        blakely_enable  :  in STD_LOGIC;
 
         -- Outputs
-        result      : out STD_LOGIC_VECTOR(C_block_size - 1 downto 0)
+        result          : out STD_LOGIC_VECTOR(C_block_size - 1 downto 0);
+        blakely_done    : out STD_LOGIC
     );
 end blakely;
 
 architecture rtl of blakely is
 
+signal bi               : STD_LOGIC;
+
 signal result_nxt       : STD_LOGIC_VECTOR(C_block_size - 1 downto 0);
-signal result_bitshift  : STD_LOGIC_VECTOR(C_block_size - 1 downto 0);
+signal result_bitshift  : STD_LOGIC_VECTOR(C_block_size downto 0);
 signal result_r         : STD_LOGIC_VECTOR(C_block_size - 1 downto 0);
 
-signal sum_a            : STD_LOGIC_VECTOR(C_block_size - 1 downto 0);
-signal mux_bi           : STD_LOGIC_VECTOR(C_block_size - 1 downto 0);
+signal sum_a            : STD_LOGIC_VECTOR(C_block_size downto 0);
+signal mux_bi           : STD_LOGIC_VECTOR(C_block_size downto 0);
 signal sum_n            : STD_LOGIC_VECTOR(C_block_size downto 0);
 signal sum_2n           : STD_LOGIC_VECTOR(C_block_size downto 0);
 
-signal overflow1        : STD_LOGIC;
-signal overflow2        : STD_LOGIC;
+signal underflow1       : STD_LOGIC;
+signal underflow2       : STD_LOGIC;
+signal mux_bi_underflow : STD_LOGIC;  
+
+signal counter          : STD_LOGIC_VECTOR(integer(ceil(log2(real(C_block_size))))-1 downto 0);
+signal blakely_done_nxt : STD_LOGIC;
 
 begin
     -- Sequential datapath
@@ -41,40 +51,66 @@ begin
     begin
         if (rst_n = '0') then
             result_r <= (others => '0');
+            counter <= (others => '1');
+            blakely_done <= '0';
         elsif (rising_edge(clk)) then
-            result_r <= result_nxt;
+            if (blakely_enable = '1') then
+                result_r <= result_nxt;
+                counter <= std_logic_vector(unsigned(counter) - 1);
+                blakely_done <= blakely_done_nxt;
+            else
+                result_r <= result_r;
+                counter <= (others => '1');
+                blakely_done <= '0';
+            end if;
         end if;
     end process;
 
-    -- Combinatorial datapath
-    process(a, bi, nega_n, nega_2n, result_r, result_nxt, result_bitshift, sum_a, mux_bi, sum_n, sum_2n, overflow1, overflow2)
+    -- Combinatorial Blakely algorithm
+    process(a, bi, n, n_shifted, result_r, result_nxt, result_bitshift, sum_a, mux_bi, sum_n, sum_2n, underflow1, underflow2, mux_bi_underflow)
     begin
-        result_bitshift <= STD_LOGIC_VECTOR(shift_left(unsigned(result_r), 1));
-        sum_a <= STD_LOGIC_VECTOR(signed(result_bitshift) + signed(a));
+        -- Calculate some intermediate values
+        result_bitshift <= STD_LOGIC_VECTOR(shift_left(resize(signed(result_r), C_block_size+1), 1));
+        sum_a <= STD_LOGIC_VECTOR(unsigned(result_bitshift) + resize(unsigned(a), C_block_size+1));
         
+        -- B[i] selection mux
         if(bi = '1') then
             mux_bi <= sum_a;
         else
             mux_bi <= result_bitshift;
         end if;
-
-        sum_n   <= STD_LOGIC_VECTOR(signed(nega_n) + signed(mux_bi));
-        sum_2n  <= STD_LOGIC_VECTOR(signed(nega_2n) + signed(mux_bi));
-        overflow1 <= sum_n(C_block_size);
-        overflow2 <= sum_2n(C_block_size);
-
-        if(overflow1 = '1' AND overflow2 = '1') then
-            result_nxt <= mux_bi;
-        elsif(overflow1 = '0' AND overflow2 = '1') then
+        
+        -- Calculate the intermediate values
+        sum_n   <= STD_LOGIC_VECTOR(unsigned(resize(signed(mux_bi), C_block_size+1)) - (resize(unsigned(n), C_block_size+1)));
+        sum_2n  <= STD_LOGIC_VECTOR(unsigned(resize(signed(mux_bi), C_block_size+1)) - (unsigned(n_shifted)));
+        mux_bi_underflow <= mux_bi(C_block_size);
+        underflow1 <= sum_n(C_block_size);
+        underflow2 <= sum_2n(C_block_size) or underflow1;
+        
+        -- Final selection mux
+        if(underflow1 = '1' AND underflow2 = '1' AND mux_bi_underflow = '0') then
+           result_nxt <= mux_bi(C_block_size - 1 downto 0);
+        elsif(underflow1 = '0' AND underflow2 = '1') then
             result_nxt <= sum_n(C_block_size - 1 downto 0);
-        elsif(overflow1 = '0' AND overflow2 = '0') then
+        elsif(underflow2 = '0' or (underflow1 = '1' AND underflow2 = '1' AND mux_bi_underflow = '1')) then
             result_nxt <= sum_2n(C_block_size - 1 downto 0);
         else
             result_nxt <= (others => '0');
         end if;
+    end process;
 
+    -- Output control signal
+    process(counter)
+    begin
+        if (counter = std_logic_vector(to_unsigned(0, integer(ceil(log2(real(C_block_size))))))) then
+            blakely_done_nxt <= '1';
+        else
+            blakely_done_nxt <= '0';
+        end if;
     end process;
     
     result <= result_r;
+    bi <= b(to_integer(unsigned(counter)));
     
 end rtl;
+
--- Vivado/Exponentiation/testbench/blakely_tb.vhd
+++ Vivado/Exponentiation/testbench/blakely_tb.vhd
@@ -1,37 +1,181 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.math_real.all;
 use ieee.numeric_std.all;
 
-entity blakely_tb is 
-    generic (
-        C_block_size : integer := 256
-    );
-end entity blakely_tb;
+library std;
+use std.textio.all;
 
-architecture blakely_tb_behave of blakely_tb is
+entity exponentiation_tb is 
+end entity exponentiation_tb;
+
+architecture blakely_tb_behave of exponentiation_tb is
+
+    constant C_block_size   : integer := 256;
+    
     -- clock and reset
     constant clk_period     : time := 10 ns;
-    signal clk              : STD_LOGIC;
-    signal rst_n            : STD_LOGIC;
+    signal clk              : STD_LOGIC := '0';
+    signal rst_n            : STD_LOGIC := '0';
 
     -- Blakely signals
-    signal a                : STD_LOGIC_VECTOR (C_block_size-1 downto 0);
-    signal b                : STD_LOGIC_VECTOR (C_block_size-1 downto 0);
-    signal bi               : STD_LOGIC; 
-    signal n                : STD_LOGIC_VECTOR(C_block_size-1 downto 0)
-    signal nega_n           : STD_LOGIC_VECTOR(C_block_size-1 downto 0)
-    signal nega_2n          : STD_LOGIC_VECTOR(C_block_size-1 downto 0)
-    signal blakely_enable   : STD_LOGIC;
+    signal a                : STD_LOGIC_VECTOR(C_block_size-1 downto 0);
+    signal b                : STD_LOGIC_VECTOR(C_block_size-1 downto 0);
+    signal n                : STD_LOGIC_VECTOR(C_block_size-1 downto 0);
+    signal n_shifted        : STD_LOGIC_VECTOR(C_block_size downto 0);
+    signal blakely_enable   : STD_LOGIC := '0';
     signal result           : STD_LOGIC_VECTOR (C_block_size-1 downto 0);
     signal blakely_done     : STD_LOGIC;
+    
+    -- Control signals
+    signal expected : std_logic_vector(C_block_size-1 downto 0);    
+    
+    -----------------------------------------------------------------------------
+    -- Function for converting from hex strings to std_logic_vector.
+    -----------------------------------------------------------------------------
+	function str_to_stdvec(inp: string) return std_logic_vector is
+		variable temp: std_logic_vector(4*inp'length-1 downto 0) := (others => 'X');
+		variable temp1 : std_logic_vector(3 downto 0);
+	begin
+		for i in inp'range loop
+			case inp(i) is
+				 when '0' =>	 temp1 := x"0";
+				 when '1' =>	 temp1 := x"1";
+				 when '2' =>	 temp1 := x"2";
+				 when '3' =>	 temp1 := x"3";
+				 when '4' =>	 temp1 := x"4";
+				 when '5' =>	 temp1 := x"5";
+				 when '6' =>	 temp1 := x"6";
+				 when '7' =>	 temp1 := x"7";
+				 when '8' =>	 temp1 := x"8";
+				 when '9' =>	 temp1 := x"9";
+				 when 'A'|'a' => temp1 := x"a";
+				 when 'B'|'b' => temp1 := x"b";
+				 when 'C'|'c' => temp1 := x"c";
+				 when 'D'|'d' => temp1 := x"d";
+				 when 'E'|'e' => temp1 := x"e";
+				 when 'F'|'f' => temp1 := x"f";
+				 when others =>  temp1 := "XXXX";
+			end case;
+			temp(4*(i-1)+3 downto 4*(i-1)) := temp1;
+		end loop;
+		return temp;
+	end function str_to_stdvec;
 
-    -- FSM signals
-    type states is (LOAD, WAIT, VALIDATE);
-    signal tb_state         : states := LOAD;
-    signal tb_state_nxt     : states := LOAD;
-
+	-----------------------------------------------------------------------------
+	-- Function for converting from std_logic_vector to a string.
+	-----------------------------------------------------------------------------
+	function stdvec_to_string ( inp1: std_logic_vector) return string is
+		variable p : string (inp1'length/4 downto 1) := (others => NUL);
+		variable nibble : std_logic_vector(3 downto 0);
+	begin
+		for i in p'length downto 1 loop
+			nibble := inp1(i*4-1 downto (i-1)*4);
+			case nibble is
+				when "0000" => p(i) := '0';
+				when "0001" => p(i) := '1';
+				when "0010" => p(i) := '2';
+				when "0011" => p(i) := '3';
+				when "0100" => p(i) := '4';
+				when "0101" => p(i) := '5';
+				when "0110" => p(i) := '6';
+				when "0111" => p(i) := '7';
+				when "1000" => p(i) := '8';
+				when "1001" => p(i) := '9';
+				when "1010" => p(i) := 'A';
+				when "1011" => p(i) := 'B';
+				when "1100" => p(i) := 'C';
+				when "1101" => p(i) := 'D';
+				when "1110" => p(i) := 'E';
+				when "1111" => p(i) := 'F';
+				when others => p(i) := 'X';
+			end case;
+		end loop;
+		return p;
+	end function;
+	
+    
 begin
 
+	-- Testbench procedure
+    process
+    begin
+      
+        report "********************************************************************************";
+        report "STARTING FIRST TESTCASE";
+        report "********************************************************************************";
+            
+        -- Set up input signals
+        n <= x"99925173ad65686715385ea800cd28120288fc70a9bc98dd4c90d676f8ff768d"; --str_to_stdvec("99925173ad65686715385ea800cd28120288fc70a9bc98dd4c90d676f8ff768d");
+        a <= x"85ee722363960779206a2b37cc8b64b5fc12a934473fa0204bbaaf714bc90c01"; --str_to_stdvec("85ee722363960779206a2b37cc8b64b5fc12a934473fa0204bbaaf714bc90c01");
+        b <= x"85ee722363960779206a2b37cc8b64b5fc12a934473fa0204bbaaf714bc90c01"; --str_to_stdvec("85ee722363960779206a2b37cc8b64b5fc12a934473fa0204bbaaf714bc90c01");
+        expected <= x"80291d6bcaeec4accc8cadf9dcc350b3c13dad526cec43bdb3d72f2d4628f697"; --str_to_stdvec("80291d6bcaeec4accc8cadf9dcc350b3c13dad526cec43bdb3d72f2d4628f697");
+        
+        -- Waiting for different stuff
+        wait until rst_n = '1';
+        wait until rising_edge(clk);
+        blakely_enable <= '1';
+        wait until blakely_done = '1';
+        wait until rising_edge(clk);
+        blakely_enable <= '0';
+
+        -- Check the result;
+        assert (result = expected)
+            report "Output message differs from the expected result"
+            severity Failure;
+            
+        report "********************************************************************************";
+        report "STARTING NEXT TESTCASE";
+        report "********************************************************************************";
+            
+        -- Set up input signals
+        n <= x"99925173ad65686715385ea800cd28120288fc70a9bc98dd4c90d676f8ff768d"; --str_to_stdvec("99925173ad65686715385ea800cd28120288fc70a9bc98dd4c90d676f8ff768d");
+        a <= x"85ee722363960779206a2b37cc8b64b5fc12a934473fa0204bbaaf714bc90c01"; --str_to_stdvec("85ee722363960779206a2b37cc8b64b5fc12a934473fa0204bbaaf714bc90c01");
+        b <= x"85ee722363960779206a2b37cc8b64b5fc12a934473fa0204bbaaf714bc90c01"; --str_to_stdvec("85ee722363960779206a2b37cc8b64b5fc12a934473fa0204bbaaf714bc90c01");
+        expected <= x"80291d6bcaeec4accc8cadf9dcc350b3c13dad526cec43bdb3d72f2d4628f697"; --str_to_stdvec("80291d6bcaeec4accc8cadf9dcc350b3c13dad526cec43bdb3d72f2d4628f697");
+        
+        -- Waiting for different stuff
+        wait until rst_n = '1';
+        wait until rising_edge(clk);
+        blakely_enable <= '1';
+        wait until blakely_done = '1';
+        wait until rising_edge(clk);
+        blakely_enable <= '0';
+
+        -- Check the result;
+        assert (result = expected)
+            report "Output message differs from the expected result"
+            severity Failure;
+    
+        -- Report done and wait forever
+        assert true;
+            report "********************************************************************************";
+            report "ALL TESTS FINISHED SUCCESSFULLY";
+            report "********************************************************************************";
+        wait;
+    end process;
+	
+	-- clock generation 
+    clk_gen: process is
+	begin
+		clk <= '1';
+		wait for clk_period / 2;
+		clk <= '0';
+		wait for clk_period / 2;
+	end process;
+    
+    -- reset_n generator
+    reset_gen: process is
+    begin
+        rst_n <= '0';
+        wait for 20 ns;
+        rst_n <= '1';
+        wait;
+    end process;
+    
+    -- Generate shifted n
+    n_shifted <= STD_LOGIC_VECTOR(shift_left(resize(unsigned(n), C_block_size+1), 1));
+   
     -- Instance of Blakely module (DUT)
     i_blakely : entity work.blakely
     generic map (
@@ -43,149 +187,12 @@ begin
 
         a               => a,
         b               => b,
-        nega_n          => nega_n,
-        nega_2n         => nega_2n,
+        n               => std_logic_vector(n),
+        n_shifted       => n_shifted,
         blakely_enable  => blakely_enable,
 
         result          => result,
         blakely_done    => blakely_done
-    )
+    );
 
--- clock generation 
-clk <= not clk after clk_period / 2;
-
--- reset_n generator
-reset_gen: process is
-begin
-    reset_n <= '0';
-    wait for 20 ns;
-    reset_n <= '1';
-    wait;
-end process;
-
--- Generate negative n
-nega_n <= STD_LOGIC_VECTOR(-signed(n));
-nega_n <= STD_LOGIC_VECTOR(-signed(shift_left(n, 1)));
-
--- Function for converting std_vector to string
-function stdvec_to_string ( a: std_logic_vector) return string is
-    variable b : string (a'length/4 downto 1) := (others => NUL);
-    variable nibble : std_logic_vector(3 downto 0);
-begin
-    for i in b'length downto 1 loop
-        nibble := a(i*4-1 downto (i-1)*4);
-        case nibble is
-            when "0000" => b(i) := '0';
-            when "0001" => b(i) := '1';
-            when "0010" => b(i) := '2';
-            when "0011" => b(i) := '3';
-            when "0100" => b(i) := '4';
-            when "0101" => b(i) := '5';
-            when "0110" => b(i) := '6';
-            when "0111" => b(i) := '7';
-            when "1000" => b(i) := '8';
-            when "1001" => b(i) := '9';
-            when "1010" => b(i) := 'A';
-            when "1011" => b(i) := 'B';
-            when "1100" => b(i) := 'C';
-            when "1101" => b(i) := 'D';
-            when "1110" => b(i) := 'E';
-            when "1111" => b(i) := 'F';
-            when others => b(i) := 'X';
-        end case;
-    end loop;
-    return b;
-end function;
-
--- Function for converting string to std_vector
-function str_to_stdvec(inp: string) return std_logic_vector is
-    variable temp: std_logic_vector(4*inp'length-1 downto 0) := (others => 'X');
-    variable temp1 : std_logic_vector(3 downto 0);
-begin
-    for i in inp'range loop
-        case inp(i) is
-             when '0' =>	 temp1 := x"0";
-             when '1' =>	 temp1 := x"1";
-             when '2' =>	 temp1 := x"2";
-             when '3' =>	 temp1 := x"3";
-             when '4' =>	 temp1 := x"4";
-             when '5' =>	 temp1 := x"5";
-             when '6' =>	 temp1 := x"6";
-             when '7' =>	 temp1 := x"7";
-             when '8' =>	 temp1 := x"8";
-             when '9' =>	 temp1 := x"9";
-             when 'A'|'a' => temp1 := x"a";
-             when 'B'|'b' => temp1 := x"b";
-             when 'C'|'c' => temp1 := x"c";
-             when 'D'|'d' => temp1 := x"d";
-             when 'E'|'e' => temp1 := x"e";
-             when 'F'|'f' => temp1 := x"f";
-             when others =>  temp1 := "XXXX";
-        end case;
-        temp(4*(i-1)+3 downto 4*(i-1)) := temp1;
-    end loop;
-    return temp;
-end function str_to_stdvec;
-
-procedure is
-begin
-    report "********************************************************************************";
-	report "STARTING NEW TESTCASE";
-	report "********************************************************************************";
-    
-    -- Set up input signals
-    n <= str_to_stdvec("99925173ad65686715385ea800cd28120288fc70a9bc98dd4c90d676f8ff768d");
-    a <= str_to_stdvec("85ee722363960779206a2b37cc8b64b5fc12a934473fa0204bbaaf714bc90c01");
-    b <= str_to_stdvec("85ee722363960779206a2b37cc8b64b5fc12a934473fa0204bbaaf714bc90c01");
-    variable expected := str_to_stdvec("171d075bdfd834ddf0433a8092d9fb9b823c353b651d89fea0079588538cbb8a")
-    
-    -- Waiting for different stuff
-    wait until rst_n = '1';
-    wait until rising_edge(clk);
-    blakely_enable <= '1';
-    wait until blakely_done = '1';
-
-    -- Check the result;
-    assert (result = expected)
-        report "Output message differs from the expected result"
-		everity Failure;
-
-    -- Report done and wait forever
-    report "********************************************************************************";
-	report "ALL TESTS FINISHED SUCCESSFULLY";
-	report "********************************************************************************";
-    wait;
-end procedure;
-
-/*
--- case 1 
-procedure case1(
-    -- signal list
-    result, result_nxt 
-)is 
-    -- var list 
-    variable temp   :   integer;
-begin
-    a   <= (others => '0');
-    bi  <= 0; 
-    result_r <= result_nxt;
-    -- wait for 5*clk_period; 
-    temp <= to_integer(a);
-    report "The value of a is" & integer'image(a);  -- prints a if a is an integer
-end case1;
-
-procedure case2(
-    result, result
-)
-
-
--- testcase 
-    testcase_1 : process(clk)
-        begin
-             
-
-        end process
-        */
 end blakely_tb_behave;
-
-
